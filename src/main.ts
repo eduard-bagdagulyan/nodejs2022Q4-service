4d5cb55d1e8f5f2e5424620f5a88462d0d8fc9c7
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2023-02-05 11:52:26 +0400

    added ConfigModule for env support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT', '4000');
+
+  await app.listen(port, () => {
+    console.log(`App started listening on http://localhost:${port}`);
+  });
 }
+
 bootstrap();
