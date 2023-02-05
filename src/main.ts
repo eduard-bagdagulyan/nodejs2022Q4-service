76e29525f26389265ddaba1211b59b4936bd37d1
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2023-02-05 15:37:10 +0400

    added ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
