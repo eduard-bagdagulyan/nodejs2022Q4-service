9e0ed36214b8b07a55a63956aecce3b6c7f531df
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2023-02-20 15:10:35 +0400

    migrated artists module

---
+++ src/artists/entities/artist.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('artists')
+export class ArtistEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('boolean')
+  grammy: boolean;
+}
